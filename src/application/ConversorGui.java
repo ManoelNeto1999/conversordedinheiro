dbb91dbda80c7e7c705b7c1d26b605743a9c306c
Manoel Neto <manoelnetodeveloper@gmail.com>
2023-09-25 18:43:26 -0300

    Adicionada opção de escolher para qual moeda deseja converter

--- src/application/ConversorGui.java
+++ src/application/ConversorGui.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,7 +22,7 @@ public class ConversorGui extends JFrame{
 	public ConversorGui() {
 		super("ConvertMoney");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(400,400);
+		setSize(500,500);
 		setLocationRelativeTo(null);
 		
 		addComponents();
@@ -59,7 +60,7 @@ public class ConversorGui extends JFrame{
 		convertPanel.add(reaisField);
 		
 		//dinheiro em dollar
-		JLabel dollarLabel = new JLabel("Dollares: ");
+		JLabel dollarLabel = new JLabel("Dollar: ");
 		dollarLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 		
 		JTextField dollarField = new JTextField(15);
@@ -73,11 +74,36 @@ public class ConversorGui extends JFrame{
 		convertPanel.add(dollarLabel);
 		convertPanel.add(dollarField);
 		
+		// campo para selecionar para qual moeda deseja fazer o cambio
+		JLabel selectionLabel = new JLabel("Escolha para qual moeda deseja converter:");
+		selectionLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+		
+		String coins[] = {"Dollar", "Euro", "Libra", "Peso"};
+		
+		JComboBox selectionBox = new JComboBox(coins);
+		selectionBox.setFont(new Font("Dialog", Font.PLAIN, 18));
+		selectionBox.setSelectedItem(coins[0]);
+		selectionBox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectionOption = (String) selectionBox.getSelectedItem();
+				dollarLabel.setText(selectionOption + ": ");
+			}
+		});
+		
+		springLayout.putConstraint(SpringLayout.WEST, selectionLabel, 35, SpringLayout.WEST, convertPanel);
+		springLayout.putConstraint(SpringLayout.NORTH, selectionLabel, 90, SpringLayout.NORTH, convertPanel);
+		springLayout.putConstraint(SpringLayout.WEST,selectionBox, 35, SpringLayout.WEST, convertPanel);
+		springLayout.putConstraint(SpringLayout.NORTH, selectionBox, 120, SpringLayout.NORTH, convertPanel);
+		
+		convertPanel.add(selectionLabel);
+		convertPanel.add(selectionBox);
+		
 		//botao de converter
 		JButton convertButton = new JButton("Converter");
 		convertButton.setFont(new Font("Dialog", Font.BOLD, 18));
 		springLayout.putConstraint(SpringLayout.WEST, convertButton, 135, SpringLayout.WEST, convertPanel);
-		springLayout.putConstraint(SpringLayout.NORTH, convertButton, 150, SpringLayout.WEST, convertPanel);
+		springLayout.putConstraint(SpringLayout.NORTH, convertButton, 160, SpringLayout.WEST, convertPanel);
 		convertButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -87,8 +113,24 @@ public class ConversorGui extends JFrame{
 				
 				if (reaisField != null) {
 					cambio = new Cambio(Double.parseDouble(number));
-					String toStringResult = cambio.toString();
-					dollarField.setText(toStringResult);
+					//String toStringResult = cambio.toString();
+					//dollarField.setText(toStringResult);
+					if (selectionBox.getSelectedItem().equals("Dollar")) {
+						String toStringResult = cambio.toString();
+						dollarField.setText(toStringResult);
+					}
+					else if (selectionBox.getSelectedItem().equals("Euro")) {
+						String toStringResult = cambio.toString1();
+						dollarField.setText(toStringResult);
+					}
+					else if (selectionBox.getSelectedItem().equals("Libra")) {
+						String toStringResult = cambio.toString2();
+						dollarField.setText(toStringResult);
+					}
+					else if (selectionBox.getSelectedItem().equals("Peso")) {
+						String toStringResult = cambio.toString3();
+						dollarField.setText(toStringResult);
+					}
 				}
 			}
 		});
--- src/entities/Cambio.java
+++ src/entities/Cambio.java
@@ -19,11 +19,34 @@ public class Cambio {
 	}
 	
 	public Double convertionDollar() {
-		return real * 0.2031;
+		return real * 0.20;
 	}
-
+	
+	public Double convertionEuro() {
+		return real * 0.19;
+	}
+	
+	public Double convertionLibra() {
+		return real * 6.24;
+	}
+	
+	public Double convertionPeso() {
+		return real * 70.90;
+	}
 	@Override
 	public String toString() {
 		return String.format("%.2f", convertionDollar());
+	}
+	
+	public String toString1() {
+		return String.format("%.2f", convertionEuro());
+	}
+	
+	public String toString2() {
+		return String.format("%.2f", convertionLibra());
+	}
+	
+	public String toString3() {
+		return String.format("%.2f", convertionPeso());
 	}
 }
